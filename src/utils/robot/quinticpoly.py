821511587fc788a6c01b10db264963e958fc3291
Zhaoyuan Ma <zma3@wpi.edu>
2022-07-22 15:08:23 -0400

    fixed the interpolation problem, need to work on actionlib

--- src/utils/robot/quinticpoly.py
+++ src/utils/robot/quinticpoly.py
@@ -9,6 +9,6 @@ def quinticpoly(t0, tf, q0, qf, qd0, qdf, qdd0, qddf):
                   [1, tf, tf**2, tf**3, tf**4, tf**5],\
                   [0, 1, 2*tf, 3*tf**2, 4*tf**3, 5*tf**4],\
                   [0, 0, 2, 6*tf, 12*tf**2, 20*tf**3]])
-    q = np.transpose(np.array([q0, qf, qd0, qdf, qdd0, qddf]))
+    q = np.transpose(np.array([q0, qd0, qdd0, qf, qdf, qddf]))
     a = np.dot(np.linalg.pinv(m), q)
     return a
